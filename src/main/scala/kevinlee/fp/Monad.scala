3e95f3ee410484a548ca1a961a5fa4c48183f796
Kevin Lee <kevin.code@kevinlee.io>
2019-04-27 23:54:17 +1000

    Close #2 - Add functor, applicative and monad laws Added: Monad laws

--- src/main/scala/kevinlee/fp/Monad.scala
+++ src/main/scala/kevinlee/fp/Monad.scala
@@ -16,4 +16,23 @@ trait Monad[M[_]] extends Applicative[M] {
       map(f)(fab => fab(a))
     }
 
+  trait MonadLaw extends ApplicativeLaw {
+    /*
+     * return a >>= f === f a
+     */
+    def leftIdentity[A, B](a: A, f: A => M[B])(implicit MB: Equal[M[B]]): Boolean =
+      MB.equal(flatMap(pure(a))(f), f(a))
+
+    /*
+     * m >>= return === m
+     */
+    def rightIdentity[A](a: M[A])(implicit MA: Equal[M[A]]): Boolean =
+      MA.equal(flatMap(a)(pure(_: A)), a)
+
+    /*
+     * (m >>= f) >>= g === m >>= (\x -> f x >>= g)
+     */
+    def associativity[A, B, C](a: M[A], f: A => M[B], g: B => M[C])(implicit MC: Equal[M[C]]): Boolean =
+      MC.equal(flatMap(flatMap(a)(f))(g), flatMap(a)(x => flatMap(f(x))(g)))
+  }
 }
