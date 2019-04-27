8856e479482c0737f13cba9a0e49571e8f76c762
Kevin Lee <kevin.code@kevinlee.io>
2019-04-27 23:44:37 +1000

    Changed: F to M in Monad

--- src/main/scala/kevinlee/fp/Monad.scala
+++ src/main/scala/kevinlee/fp/Monad.scala
@@ -4,15 +4,15 @@ package kevinlee.fp
   * @author Kevin Lee
   * @since 2019-03-16
   */
-trait Monad[F[_]] extends Applicative[F] {
+trait Monad[M[_]] extends Applicative[M] {
 
-  def map[A, B](fa: F[A])(f: A => B): F[B] =
-    flatMap(fa)(a => pure(f(a)))
+  override def map[A, B](ma: M[A])(f: A => B): M[B] =
+    flatMap(ma)(a => pure(f(a)))
 
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+  def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
 
-  def ap[A, B](fa: => F[A])(f: => F[A => B]): F[B] =
-    flatMap(fa) { a =>
+  override def ap[A, B](ma: => M[A])(f: => M[A => B]): M[B] =
+    flatMap(ma) { a =>
       map(f)(fab => fab(a))
     }
 
