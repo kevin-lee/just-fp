358389076f70cd5aa1e50b5ef5b4152347d1f933
Kevin Lee <kevin.code@kevinlee.io>
2019-08-04 02:30:32 +1000

    Prepare to release 0.5.0

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -9,7 +9,7 @@ object ProjectInfo {
   val ProjectScalaVersion: String = "2.13.0"
   val CrossScalaVersions: Seq[String] = Seq("2.10.7", "2.11.12", "2.12.8", ProjectScalaVersion)
 
-  val ProjectVersion: String = "0.4.0"
+  val ProjectVersion: String = "0.5.0"
 
   val commonWarts: Seq[wartremover.Wart] = Warts.allBut(Wart.DefaultArguments, Wart.Overloading, Wart.Any, Wart.Nothing, Wart.NonUnitStatements)
 
