0b6a104821d2092d301d2d39ed6194d2b03a1be2
Kevin Lee <kevin.code@kevinlee.io>
2019-09-17 17:36:29 +1000

    Close #50 - Renamed: package kevinlee => just

---
+++ src/main/scala/just/fp/JustSyntax.scala
@@ -0,0 +1,9 @@
+package just.fp
+
+import just.fp.syntax.{EitherSyntax, EqualSyntax}
+
+/**
+  * @author Kevin Lee
+  * @since 2019-05-26
+  */
+object JustSyntax extends EqualSyntax with EitherSyntax
