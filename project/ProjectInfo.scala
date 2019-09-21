a822e490008d239fde37d57166c1f8ef7754f388
Kevin Lee <kevin.code@kevinlee.io>
2019-09-21 13:19:58 +1000

    Updated: build.sbt and build.properties - Scala 2.13.0 => 2.13.1 - sbt 1.2.8 => 1.3.1

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -6,11 +6,12 @@ import wartremover.WartRemover.autoImport.{Wart, Warts}
   */
 object ProjectInfo {
 
-  val ProjectScalaVersion: String = "2.13.0"
+  val ProjectScalaVersion: String = "2.13.1"
   val CrossScalaVersions: Seq[String] = Seq("2.10.7", "2.11.12", "2.12.10", ProjectScalaVersion)
 
   val ProjectVersion: String = "1.1.0"
 
-  val commonWarts: Seq[wartremover.Wart] = Warts.allBut(Wart.DefaultArguments, Wart.Overloading, Wart.Any, Wart.Nothing, Wart.NonUnitStatements)
+  val commonWarts: Seq[wartremover.Wart] =
+    Warts.allBut(Wart.DefaultArguments, Wart.Overloading, Wart.Any, Wart.Nothing, Wart.NonUnitStatements)
 
 }
