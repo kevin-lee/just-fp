4417ab58dd19c9a6f1deb8e719bab3ea36d0363e
Kevin Lee <kevin.code@kevinlee.io>
2019-10-02 16:03:07 +1000

    Close #89 - Add writer syntax

---
+++ src/main/scala/just/fp/syntax/WriterSyntax.scala
@@ -0,0 +1,22 @@
+package just.fp.syntax
+
+import just.fp.Writer
+
+import scala.language.implicitConversions
+
+/**
+  * @author Kevin Lee
+  * @since 2019-10-02
+  */
+object WriterOps {
+  final class ToWriter[A](val a: A) extends AnyVal {
+    def writer[W](w: W): Writer[W, A] = Writer(w, a)
+  }
+}
+
+trait WriterSyntax {
+  import WriterOps._
+
+  @SuppressWarnings(Array("org.wartremover.warts.ImplicitConversion"))
+  implicit final def toWriter[A](a: A): ToWriter[A] = new ToWriter(a)
+}
