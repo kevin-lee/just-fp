d02bd27142e8c615a03b9a0b1e21ab964f6ce260
Kevin Lee <kevin.code@kevinlee.io>
2020-08-08 21:02:42 +1000

    Close #170 - Build for Dotty

--- .github/workflows/sbt-build.sh
+++ .github/workflows/sbt-build.sh
@@ -8,21 +8,22 @@ if [ -z "$2" ]
     echo "sbt-build.sh core 2.12.10"
     exit 1
 else
+  : ${CURRENT_BRANCH_NAME:?"CURRENT_BRANCH_NAME is missing."}
+  : ${CI_BRANCH:?"CI_BRANCH is missing."}
   project_name=$1
   scala_version=$2
   echo "============================================"
   echo "Build projects"
   echo "--------------------------------------------"
   echo ""
-  export CI_BRANCH="${GITHUB_REF#refs/heads/}"
-  if [[ "$CI_BRANCH" == "main" || "$CI_BRANCH" == "release" ]]
+  if [[ "$CURRENT_BRANCH_NAME" == "main" || "$CURRENT_BRANCH_NAME" == "release" ]]
   then
-    sbt -J-Xmx2048m "; project ${project_name}; ++ ${scala_version}!; clean; coverage; test; coverageReport; coverageAggregate"
-    sbt -J-Xmx2048m "; project ${project_name}; ++ ${scala_version}!; coveralls"
-    sbt -J-Xmx2048m "; project ${project_name}; ++ ${scala_version}!; clean; packagedArtifacts"
+    sbt -J-Xmx2048m "project ${project_name}" ++${scala_version}! clean coverage test coverageReport coverageAggregate
+    sbt -J-Xmx2048m "project ${project_name}" ++${scala_version}! coveralls
+    sbt -J-Xmx2048m "project ${project_name}" ++${scala_version}! clean packagedArtifacts
   else
-    sbt -J-Xmx2048m "; project ${project_name}; ++ ${scala_version}!; clean; coverage; test; coverageReport; coverageAggregate; package"
-    sbt -J-Xmx2048m "; project ${project_name}; ++ ${scala_version}!; coveralls"
+    sbt -J-Xmx2048m "project ${project_name}" ++${scala_version}! clean coverage test coverageReport coverageAggregate package
+    sbt -J-Xmx2048m "project ${project_name}" ++${scala_version}! coveralls
   fi
 
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,9 +2,9 @@ logLevel := sbt.Level.Warn
 
 addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.5")
 
-addSbtPlugin("org.wartremover" % "sbt-wartremover" % "2.4.5")
+addSbtPlugin("org.wartremover" % "sbt-wartremover" % "2.4.10")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.1")
 
 addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.2.7")
 
@@ -13,3 +13,5 @@ addSbtPlugin("io.kevinlee" % "sbt-devoops" % "1.0.3")
 addSbtPlugin("com.47deg" % "sbt-microsites" % "1.1.4")
 
 addSbtPlugin("io.kevinlee" % "sbt-docusaur" % "0.1.3")
+
+addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.4.1")
